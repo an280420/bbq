de0141545ab0d9a19aa095ddb718fad62e239136
@an280420 <angit280420@gmail.com>
2021-01-25 01:46:02 +0500

    comment configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,10 +56,10 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Log disallowed deprecations.
-  config.active_support.disallowed_deprecation = :log
+  # config.active_support.disallowed_deprecation = :log
 
   # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
+  # config.active_support.disallowed_deprecation_warnings = []
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
