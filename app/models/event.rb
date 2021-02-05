3793bfe4a864a41bdec97faae62a188dff9cacb1
@an280420 <angit280420@gmail.com>
2021-02-06 01:52:23 +0500

    create comment model and view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :user, presence: true
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
-  validates :datetime, presence: true    
+  validates :datetime, presence: true
 end
