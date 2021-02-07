6c5551e6f0cdee7d7b73ccfa7c4b7a6a75a963e0
@an280420 <angit280420@gmail.com>
2021-02-07 17:54:43 +0500

    edit user_id

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_223059) do
     t.string "user_name"
     t.string "user_email"
     t.integer "event_id", null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_subscriptions_on_event_id"
