09d4de6f7dbd4bbb570de711b80ec905ff8ac1cb
@an280420 <angit280420@gmail.com>
2021-02-10 02:18:49 +0500

    Add prohibiting subscription for owner

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,11 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :event, presence: true
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-
+  validates :user, uniqueness: {scope: :event_id}, exclusion: { in: :event_user }, if: -> { user.present? }
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
   validates :user_email, exclusion: { in: User.all.map(&:email) }, unless: -> { user.present? }
 
   def user_name
@@ -24,5 +23,11 @@ class Subscription < ApplicationRecord
     else
       super
     end
+  end
+
+  private
+
+  def event_user
+    [event.user]
   end
 end
