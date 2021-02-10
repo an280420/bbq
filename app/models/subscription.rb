9781e19da854992cc279e2d89900dd00ee76862b
@an280420 <angit280420@gmail.com>
2021-02-10 12:29:01 +0500

    Add ban subscription for event owner

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,13 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user, uniqueness: {scope: :event_id}, exclusion: { in: :event_user }, if: -> { user.present? }
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
-  validate :email_exists, unless: -> { user.present? }
+  validate :email_exists?, unless: -> { user.present? }
+  validate :is_subscriber_owner_of_event?, if: -> { user.present? }
 
   def user_name
     if user.present?
@@ -27,13 +28,15 @@ class Subscription < ApplicationRecord
 
   private
 
-  def event_user
-    [event.user]
-  end
-
-  def email_exists
+  def email_exists?
     if User.exists?(email: user_email)
       errors.add(:user_email, :email_of_exsisting_user)
+    end
+  end
+
+  def is_subscriber_owner_of_event?
+    if user == event.user
+      errors.add(:user, :subcsriber_is_owner_of_event)
     end
   end
 end
