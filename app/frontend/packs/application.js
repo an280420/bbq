f6fff07d7e63e9657d35c6634a071f997b3267a1
@an280420 <angit280420@gmail.com>
2021-02-27 14:03:02 +0500

    Add lightbox

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,9 +5,14 @@ import "channels"
 Rails.start()
 ActiveStorage.start()
 
+var jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 import 'bootstrap'
 import '../styles/application'
 import '../scripts/application'
 import '../scripts/maps'
+import '../scripts/lightbox'
 
 const images = require.context('../images', true)
