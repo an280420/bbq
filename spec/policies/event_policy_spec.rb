50b905d603242f739a3a5bbece347aeaa54d1390
@an280420 <angit280420@gmail.com>
2021-03-17 18:50:10 +0500

    Added permission for policy action

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -2,26 +2,36 @@ require 'rails_helper'
 
 RSpec.describe EventPolicy, type: :policy do
   let(:user) { User.new }
+  let(:event) { Event.new(user: user) }
+  let(:other_user) { User.new }
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user.events << event
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :edit? do
+    it { is_expected.to permit(user, event) }
+    it { is_expected.not_to permit(nil, event) }
+    it { is_expected.not_to permit(other_user, event) }
   end
 
   permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it { is_expected.to permit(user, event) }
+    it { is_expected.not_to permit(nil, event) }
+    it { is_expected.not_to permit(other_user, event) }
+  end
+  
+  permissions :destroy? do
+    it { is_expected.to permit(user, event) }
+    it { is_expected.not_to permit(nil, event) }
+    it { is_expected.not_to permit(other_user, event) }
   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :show? do
+    it { is_expected.to permit(user, event) }
+    it { is_expected.not_to permit(nil, event) }
+    it { is_expected.not_to permit(other_user, event) }
   end
 end
