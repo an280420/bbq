2ad39f0d4aaaf6307dc5cd6c81840a581fffb68b
@an280420 <angit280420@gmail.com>
2021-06-02 17:00:49 +0500

    Change server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "eventsboard.site", user: "deploy", roles: %w[app db web resque_worker]
+server "188.166.86.210", user: "deploy", roles: %w[app db web resque_worker]
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
